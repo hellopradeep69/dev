c0c80710865e0f4e2c5c694c77f6305e08696cc7
hellopradeep69 <hellopradeep69@gmail.com>
2025-12-03 22:27:38 +0530

    10/home/hellopradeep/dev/Arch.sh completed

--- Arch.sh
+++ Arch.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 # My development setup for linux Arch
 
-Install_pack(){
+Install_pack() {
     sudo pacman -Syu
-    sudo pacman -S --noconfirm fastfetch ghostty fd ripgrep tmux zsh btop curl wget trash-cli gcc
+    sudo pacman -S --noconfirm fastfetch ghostty fd ripgrep tmux zsh btop curl wget trash-cli gcc nodejs npm
 }
 
-Install_font(){
+Install_font() {
     sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-terminus-nerd ttf-jetbrains-mono
     fc-cache -f -v
 }
 
-Install_zplugin(){
+Install_zplugin() {
 
-# Create the directory if missing
-mkdir -p ~/.oh-my-zsh/custom/plugins
+    # Create the directory if missing
+    mkdir -p ~/.oh-my-zsh/custom/plugins
 
-# Install plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions \
-  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    # Install plugins
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting \
-  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+        ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-completions \
-  ~/.oh-my-zsh/custom/plugins/zsh-completions
+    git clone https://github.com/zsh-users/zsh-completions \
+        ~/.oh-my-zsh/custom/plugins/zsh-completions
 }
 
-Zsh_setup(){
+Zsh_setup() {
     Shell=$(echo "$SHELL")
     if [[ $Shell == "/usr/bin/zsh" ]]; then
         echo "zsh"
     else
         echo "not zsh"
-        chsh -s  $(which zsh)
+        chsh -s $(which zsh)
     fi
 
-    if [[  -d  "$HOME/.oh-my-zsh" ]]; then
+    if [[ -d "$HOME/.oh-my-zsh" ]]; then
         echo "zsh available not touching"
     else
         echo "Downloading oh my zsh"
@@ -49,7 +49,12 @@ Zsh_setup(){
 
 }
 
-Nvim_setup(){
+Tmux_in() {
+    trash $HOME/.tmux.conf
+    cp -r $HOME/dev/.tmux.conf $HOME/.tmux.conf
+}
+
+Nvim_setup() {
     rm -rf ~/.config/nvim
     mkdir -p ~/.config/nvim
     git clone https://github.com/hellopradeep69/nvim.git ~/.config/nvim/
@@ -57,22 +62,19 @@ Nvim_setup(){
     rm -rf ~/.config/nvim/README.md
 }
 
-Ghostty_setup(){
-    echo "Resetting ghostty config" 
+Ghostty_setup() {
+    echo "Resetting ghostty config"
     rm -rf ~/.config/ghostty
     mkdir -p ~/.config/ghostty
     cp -r $HOME/dev/resource/ghostty/config ~/.config/ghostty/
 }
 
-Install_sway(){
-    sudo pacman -Syu
-    sudo pacman -S sway swaylock i3status rofi-wayland brightnessctl mako wl-clipboard cliphist
+Main() {
+    Ghostty_setup
+    Install_font
+    Install_pack
+    Tmux_in
+    Nvim_setup
+    Zsh_setup
+
 }
-
-# Ghostty_setup
-# Install_font
-Install_pack
-Install_sway
-# Nvim_setup
-# Zsh_setup
-
---
+++ sway.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Install Sway | i3wm replacement
+
+Install_sway() {
+    sudo pacman -Syu
+    sudo pacman -S sway swaylock i3status rofi-wayland brightnessctl mako wl-clipboard cliphist
+}
+
+Make_dir() {
+    mkdir -p $HOME/.config/i3status/
+    mkdir -p $HOME/.config/sway/
+}
+
+Copy_it() {
+    cp -r $HOME/dev/resource/sway/. $HOME/.config/sway
+    cp -r $HOME/dev/resource/i3status/. $HOME/.config/i3status
+}
+
+Main() {
+    echo "Rofi doesnt need any like config"
+    Install_sway
+    Make_dir
+    Copy_it
+}
+
+Main
