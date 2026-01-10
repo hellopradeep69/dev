9fae6c412a26a3f40278fb10d3d07ae2012fa7f6
hellopradeep69 <hellopradeep69@gmail.com>
2026-01-10 10:50:25 +0530

    sway added

--- sway.sh
+++ sway.sh
@@ -18,6 +18,11 @@ Copy_it() {
     cp -r $HOME/dev/resource/i3status/. $HOME/.config/i3status
 }
 
+Intel_status(){
+    trash $HOME/.config/i3status/
+    cp -r /home/hellopradeep/dev/resource/i3status-intel/. $HOME/.config/i3status
+}
+
 Main() {
     echo "Rofi doesnt need any like config"
     Install_sway
@@ -30,3 +35,11 @@ Main() {
 }
 
 Main
+
+
+if [[ -f "/sys/class/power_supply/BAT0/capacity" ]]; then
+    echo "Seems like you are having intel"
+    Intel_status
+    echo "intel setup"
+fi
+
