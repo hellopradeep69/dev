bb66eaf9440a935e5238ed5ffbec2b84883747b1
k.pradeep <Hellopradeep69@gmail.com>
2026-02-13 15:08:54 +0530

    dop dop odp[;]

--- dev.sh
+++ dev.sh
@@ -10,7 +10,7 @@ Arch_pack() {
         btop curl wget trash-cli gcc nodejs npm neovim fzf github-cli tree-sitter-cli\
         telegram-desktop obs-studio acpi mpv yt-dlp eza bc w3m lazygit thunar ristretto\
         thunar-volman gvfs tumbler thunar-archive-plugin unzip imagemagick ufw\
-        man-db tig xfce4-screenshooter
+        man-db tig xfce4-screenshooter w3m
 
     echo "Installing font"
     sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-terminus-nerd ttf-jetbrains-mono
@@ -149,6 +149,7 @@ Qute_app(){
 
 W3m_config(){
     if command -v w3m >/dev/null 2>&1;then
+        mkdir -p $HOME/.w3m/
         cp -r $HOME/dev/resource/w3m/keymap $HOME/.w3m/keymap
         echo "w3m config reloaded to $HOME/.w3m/"
     fi
