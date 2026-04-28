7b695daa3332963c9208492c5e14ff964dd954a0
k.pradeep <Hellopradeep69@gmail.com>
2026-04-29 00:19:22 +0530

    Topen + w3m

--- dev.sh
+++ dev.sh
@@ -5,297 +5,297 @@ Options=$1
 Options2=$2
 
 Arch_pack() {
-    echo "Installing variour package fastfetch ghostty fd ripgrep tmux zsh btop curl wget trash-cli gcc nodejs npm "
-    sudo pacman -S --noconfirm --needed fastfetch ghostty fd ripgrep tmux zsh \
-        btop curl wget trash-cli gcc nodejs npm neovim fzf github-cli tree-sitter-cli telegram-desktop obs-studio acpi mpv yt-dlp eza bc w3m lazygit thunar ristretto thunar-volman gvfs tumbler thunar-archive-plugin unzip imagemagick ufw man-db tig xfce4-screenshooter w3m
+	echo "Installing variour package fastfetch ghostty fd ripgrep tmux zsh btop curl wget trash-cli gcc nodejs npm "
+	sudo pacman -S --noconfirm --needed fastfetch ghostty fd ripgrep tmux zsh \
+		btop curl wget trash-cli gcc nodejs npm neovim fzf github-cli tree-sitter-cli telegram-desktop obs-studio acpi mpv yt-dlp eza bc w3m lazygit thunar ristretto thunar-volman gvfs tumbler thunar-archive-plugin unzip imagemagick ufw man-db tig xfce4-screenshooter w3m
 
-    echo "Installing font"
-    sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-terminus-nerd ttf-jetbrains-mono
-    fc-cache -f -v
+	echo "Installing font"
+	sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-terminus-nerd ttf-jetbrains-mono
+	fc-cache -f -v
 }
 
 Void_pack() {
-    # soon
-    echo "voiding"
-    bash "$HOME/dev/void.sh"
+	# soon
+	echo "voiding"
+	bash "$HOME/dev/void.sh"
 }
 
 Debian_pack() {
-    echo "voiding"
-    bash "$HOME/dev/debian.sh"
+	echo "voiding"
+	bash "$HOME/dev/debian.sh"
 }
 
 Install_wallie() {
-    if [[ ! -f "$HOME/Pictures/anime_waifu.jpg" ]]; then
-        mkdir "$HOME/Pictures"
-        echo "Arch comes with anime waifu"
-        cp "$HOME/dev/resource/wallpaper/anime_waifu.jpg" "$HOME/Pictures/"
-        cp "$HOME/dev/resource/wallpaper/Arch.jpg" "$HOME/Pictures/"
-    fi
+	if [[ ! -f "$HOME/Pictures/anime_waifu.jpg" ]]; then
+		mkdir "$HOME/Pictures"
+		echo "Arch comes with anime waifu"
+		cp "$HOME/dev/resource/wallpaper/anime_waifu.jpg" "$HOME/Pictures/"
+		cp "$HOME/dev/resource/wallpaper/Arch.jpg" "$HOME/Pictures/"
+	fi
 }
 
 Zsh_setup() {
-    Shell=$(echo "$SHELL")
-    if [[ $Shell == "/usr/bin/zsh" ]]; then
-        echo "zsh"
-    else
-        echo "not zsh"
-        chsh -s $(which zsh)
-    fi
+	Shell=$(echo "$SHELL")
+	if [[ $Shell == "/usr/bin/zsh" ]]; then
+		echo "zsh"
+	else
+		echo "not zsh"
+		chsh -s $(which zsh)
+	fi
 
-    echo "Reinstalling zsh and zsh conf"
-    [[ -f "$HOME/.zshrc" ]] && trash "$HOME/.zshrc"
-    cp "$HOME/dev/.zshrc" "$HOME/.zshrc"
+	echo "Reinstalling zsh and zsh conf"
+	[[ -f "$HOME/.zshrc" ]] && trash "$HOME/.zshrc"
+	cp "$HOME/dev/.zshrc" "$HOME/.zshrc"
 
 }
 
 Tmux_in() {
-    echo ""
-    echo "Reinstalling tmux and tmux conf"
-    [[ -f "$HOME/.tmux.conf" ]] && trash "$HOME/.tmux.conf"
-    cp -r "$HOME/dev/.tmux.conf" "$HOME/.tmux.conf"
+	echo ""
+	echo "Reinstalling tmux and tmux conf"
+	[[ -f "$HOME/.tmux.conf" ]] && trash "$HOME/.tmux.conf"
+	cp -r "$HOME/dev/.tmux.conf" "$HOME/.tmux.conf"
 }
 
 Nvim_setup() {
-    echo ""
-    if [[ ! -d "$HOME/.config/nvim/" ]]; then
-        echo "Nvim setuping.."
-        rm -rf ~/.config/nvim
-        mkdir -p ~/.config/nvim
-        git clone https://github.com/hellopradeep69/nvim.git ~/.config/nvim/
-    fi
+	echo ""
+	if [[ ! -d "$HOME/.config/nvim/" ]]; then
+		echo "Nvim setuping.."
+		rm -rf ~/.config/nvim
+		mkdir -p ~/.config/nvim
+		git clone https://github.com/hellopradeep69/nvim.git ~/.config/nvim/
+	fi
 }
 
 Ghostty_setup() {
-    echo ""
-    echo "Resetting ghostty config"
-    trash ~/.config/ghostty
-    mkdir -p ~/.config/ghostty
-    cp -r $HOME/dev/resource/ghostty/. ~/.config/ghostty/
+	echo ""
+	echo "Resetting ghostty config"
+	trash ~/.config/ghostty
+	mkdir -p ~/.config/ghostty
+	cp -r $HOME/dev/resource/ghostty/. ~/.config/ghostty/
 }
 
 Install_theme() {
-    sudo pacman -S --noconfirm --needed arc-gtk-theme-eos eos-qogir-icons
+	sudo pacman -S --noconfirm --needed arc-gtk-theme-eos eos-qogir-icons
 }
 
 Zathura_install() {
-    echo ""
-    echo "Resetting Zathura config"
-    trash ~/.config/zathura
-    mkdir -p ~/.config/zathura/
-    cp -r $HOME/dev/resource/zathura/. ~/.config/zathura/
+	echo ""
+	echo "Resetting Zathura config"
+	trash ~/.config/zathura
+	mkdir -p ~/.config/zathura/
+	cp -r $HOME/dev/resource/zathura/. ~/.config/zathura/
 }
 
 Rofi_setup() {
-    echo ""
-    if [[ ! -d "$HOME/.config/rofi/" ]]; then
-        echo "Rofi Config setup"
-        mkdir -p ~/.config/rofi/
-        trash ~/.config/rofi/
-        cp -r $HOME/dev/resource/rofi/. ~/.config/rofi/
-    fi
+	echo ""
+	if [[ ! -d "$HOME/.config/rofi/" ]]; then
+		echo "Rofi Config setup"
+		mkdir -p ~/.config/rofi/
+		trash ~/.config/rofi/
+		cp -r $HOME/dev/resource/rofi/. ~/.config/rofi/
+	fi
 }
 
 Script_install() {
-    echo "Scripting Install"
+	echo "Scripting Install"
 
-    echo "trashing scripts"
-    trash "$HOME/.local/bin/"
-    mkdir -p "$HOME/.local/bin/"
+	echo "trashing scripts"
+	trash "$HOME/.local/bin/"
+	mkdir -p "$HOME/.local/bin/"
 
-    echo " "
-    cp -r $HOME/dev/resource/scripts/. $HOME/.local/bin/
-    echo "Scripts copied to $HOME/.local/bin/"
+	echo " "
+	cp -r $HOME/dev/resource/scripts/. $HOME/.local/bin/
+	echo "Scripts copied to $HOME/.local/bin/"
 }
 
 Application_install() {
-    echo "installing .desktop"
-    if [[ ! -d "$HOME/.local/share/applications" ]]; then
-        mkdir -p "$HOME/.local/share/applications"
-        echo "created dir"
-    fi
+	echo "installing .desktop"
+	if [[ ! -d "$HOME/.local/share/applications" ]]; then
+		mkdir -p "$HOME/.local/share/applications"
+		echo "created dir"
+	fi
 
-    trash "$HOME/.local/share/applications/"
+	trash "$HOME/.local/share/applications/"
 
-    #TODO: If zen command not found add application - useful for void
-    cp -r $HOME/dev/resource/application/. $HOME/.local/share/applications/
-    echo "Applications copied to $HOME/.local/share/applications"
+	#TODO: If zen command not found add application - useful for void
+	cp -r $HOME/dev/resource/application/. $HOME/.local/share/applications/
+	echo "Applications copied to $HOME/.local/share/applications"
 }
 
 Zen_install() {
-    if ! command -v zen-browser >/dev/null 2>&1; then
-        wget -qO- https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.xz | sudo tar xJ -C /opt
-        echo "Extracting zen"
-        sudo ln -sf /opt/zen/zen /usr/bin/zen-browser
-        echo "zen installed"
-    fi
+	if ! command -v zen-browser >/dev/null 2>&1; then
+		wget -qO- https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.xz | sudo tar xJ -C /opt
+		echo "Extracting zen"
+		sudo ln -sf /opt/zen/zen /usr/bin/zen-browser
+		echo "zen installed"
+	fi
 }
 
 Zen_app() {
-    echo "zenning"
-    Zen_install
+	echo "zenning"
+	Zen_install
 
-    cp -r $HOME/dev/resource/zen/. $HOME/.local/share/applications/
-    echo "zen copied to $HOME/.local/share/applications"
+	cp -r $HOME/dev/resource/zen/. $HOME/.local/share/applications/
+	echo "zen copied to $HOME/.local/share/applications"
 }
 
 Qute_app() {
-    if command -v qutebrowser >/dev/null 2>&1; then
-        cp -r $HOME/dev/resource/qutebrowser/config.py $HOME/.config/qutebrowser/config.py
-        echo "qutebrowser copied to $HOME/.local/share/applications"
-    fi
+	if command -v qutebrowser >/dev/null 2>&1; then
+		cp -r $HOME/dev/resource/qutebrowser/config.py $HOME/.config/qutebrowser/config.py
+		echo "qutebrowser copied to $HOME/.local/share/applications"
+	fi
 }
 
 W3m_config() {
-    if command -v w3m >/dev/null 2>&1; then
-        mkdir -p $HOME/.w3m/
-        cp -r $HOME/dev/resource/w3m/keymap $HOME/.w3m/keymap
-        echo "w3m config reloaded to $HOME/.w3m/"
-    fi
+	if command -v w3m >/dev/null 2>&1; then
+		mkdir -p $HOME/.w3m/
+		cp -r $HOME/dev/resource/w3m/keymap $HOME/.w3m/keymap
+		echo "w3m config reloaded to $HOME/.w3m/"
+	fi
 }
 
 Neomutt_config() {
-    if command -v neomutt >/dev/null 2>&1; then
-        mkdir -p /home/hellopradeep/.config/mutt/
-        cp -r $HOME/dev/resource/mutt/muttrc $HOME/.config/mutt/muttrc
-        echo "neomutt config reloaded to $HOME/.config/mutt"
-    fi
+	if command -v neomutt >/dev/null 2>&1; then
+		mkdir -p /home/hellopradeep/.config/mutt/
+		cp -r $HOME/dev/resource/mutt/muttrc $HOME/.config/mutt/muttrc
+		echo "neomutt config reloaded to $HOME/.config/mutt"
+	fi
 }
 
 Firewall_setup() {
-    if command -v systemctl >/dev/null 2>&1; then
-        sudo systemctl enable ufw
-        sudo systemctl start ufw
+	if command -v systemctl >/dev/null 2>&1; then
+		sudo systemctl enable ufw
+		sudo systemctl start ufw
 
-        sudo ufw default deny incoming
-        sudo ufw default allow outgoing
-    else
-        echo "activate your own firewall"
-    fi
+		sudo ufw default deny incoming
+		sudo ufw default allow outgoing
+	else
+		echo "activate your own firewall"
+	fi
 }
 
 Yay_install() {
-    if command -v yay >/dev/null 2>&1; then
-        echo ""
-        echo "Yay already downloaded"
-    else
-        sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-    fi
+	if command -v yay >/dev/null 2>&1; then
+		echo ""
+		echo "Yay already downloaded"
+	else
+		sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+	fi
 }
 
 Say_arch() {
-    echo ""
-    echo "Now You can proudly say I use arch btw"
-    echo ""
+	echo ""
+	echo "Now You can proudly say I use arch btw"
+	echo ""
 }
 
 Basic() {
-    Install_wallie
-    Tmux_in
-    Nvim_setup
-    Rofi_setup
-    Zathura_install
-    Qute_app
-    W3m_config
-    Zsh_setup
-    Script_install
-    Application_install
-    Ghostty_setup
-    echo "Installing basic stuff"
+	Install_wallie
+	Tmux_in
+	Nvim_setup
+	Rofi_setup
+	Zathura_install
+	Qute_app
+	W3m_config
+	Zsh_setup
+	Script_install
+	Application_install
+	Ghostty_setup
+	echo "Installing basic stuff"
 }
 
 Main() {
-    Install_wallie
-    Tmux_in
-    Nvim_setup
-    Firewall_setup
-    Rofi_setup
-    Zathura_install
-    Qute_app
-    W3m_config
-    Neomutt_config
-    Zsh_setup
-    Script_install
-    Application_install
-    Ghostty_setup
-    echo ""
-    echo "Basic Setup done for your linux"
+	Install_wallie
+	Tmux_in
+	Nvim_setup
+	Firewall_setup
+	Rofi_setup
+	Zathura_install
+	Qute_app
+	W3m_config
+	Neomutt_config
+	Zsh_setup
+	Script_install
+	Application_install
+	Ghostty_setup
+	echo ""
+	echo "Basic Setup done for your linux"
 }
 
 Arch_install() {
-    Arch_pack
-    Yay_install
-    Main
+	Arch_pack
+	Yay_install
+	Main
 }
 
 Void_install() {
-    Void_pack
-    Main
-    Zen_app
+	Void_pack
+	Main
+	Zen_app
 }
 
 Debian_install() {
-    Debian_pack
-    Basic
+	Debian_pack
+	Basic
 }
 
 Help() {
-    # TODO: add help
-    echo "Usage:"
-    echo "distro available : void and arch"
-    echo " "
-    echo "./dev.sh -arch -hyprland        to install hyprland and config in arch"
-    echo "./dev.sh -arch -sway            to install sway and config in arch"
-    echo "./dev.sh -arch -i3              to install i3wm and config in arch"
-    echo "./dev.sh -debian                to install debian"
-    echo "bash dev.sh -void -i3           to install i3wm and config in void"
-    echo " "
-    echo "           * void script only support i3-wm for now"
+	# TODO: add help
+	echo "Usage:"
+	echo "distro available : void and arch"
+	echo " "
+	echo "./dev.sh -arch -hyprland        to install hyprland and config in arch"
+	echo "./dev.sh -arch -sway            to install sway and config in arch"
+	echo "./dev.sh -arch -i3              to install i3wm and config in arch"
+	echo "./dev.sh -debian                to install debian"
+	echo "bash dev.sh -void -i3           to install i3wm and config in void"
+	echo " "
+	echo "           * void script only support i3-wm for now"
 }
 
 case "$Options" in
 -help)
-    Help
-    ;;
+	Help
+	;;
 -debian)
-    Debian_install
-    ;;
+	Debian_install
+	;;
 -arch)
-    Arch_install
-    ;;
+	Arch_install
+	;;
 -void)
-    Void_install
-    ;;
+	Void_install
+	;;
 -hyprland)
-    bash $HOME/dev/hyprland.sh
-    ;;
+	bash $HOME/dev/hyprland.sh
+	;;
 -sway)
-    bash $HOME/dev/sway.sh
-    ;;
+	bash $HOME/dev/sway.sh
+	;;
 -i3)
-    bash $HOME/dev/i3wm.sh
-    ;;
+	bash $HOME/dev/i3wm.sh
+	;;
 *)
-    Help
-    ;;
+	Help
+	;;
 esac
 
 case "$Options2" in
 -hyprland)
-    bash $HOME/dev/hyprland.sh
-    ;;
+	bash $HOME/dev/hyprland.sh
+	;;
 -sway)
-    bash $HOME/dev/sway.sh
-    ;;
+	bash $HOME/dev/sway.sh
+	;;
 -i3)
-    bash $HOME/dev/i3wm.sh
-    ;;
+	bash $HOME/dev/i3wm.sh
+	;;
 -yay)
-    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-    ;;
+	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+	;;
 -theme)
-    Install_theme
-    ;;
+	Install_theme
+	;;
 esac
 
 echo ""
