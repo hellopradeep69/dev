9a9f49932715737ee99bc7ee6e2938e0df8a705a
Hellopradeep69 <Hellopradeep69@gmail.com>
2026-06-09 18:30:06 +0530

    tems

--- Temp.sh
+++ Temp.sh
@@ -1,5 +1,8 @@
 sudo firewall-cmd --permanent --add-port=8096/tcp
 sudo firewall-cmd --reload
 
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --reload
+
 # sudo firewall-cmd --permanent --remove-port=8096/tcp
 # sudo firewall-cmd --reload
